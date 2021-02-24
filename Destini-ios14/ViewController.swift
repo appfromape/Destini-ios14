39b7cf2ccfe7a390761f8bdaf555340b7e421639
appfromape <appfromape@gmail.com>
2021-02-24 21:13:59 +0800

    Initial Commit

---
+++ Destini-ios14/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Destini-ios14
+//
+//  Created by 程式猿 on 2021/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
